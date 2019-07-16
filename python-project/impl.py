b27266da7379598aed50c02aef9721847a036505
sergeyt <stodyshev@gmail.com>
2019-07-16 22:46:38 +0700

    initial impl

--- python-project/impl.py
+++ python-project/impl.py
@@ -1,3 +1,30 @@
 # k is maximum number of unique chars
 def longest_substring(s, k = 2):
-    return s
+    if s is None:
+        return s
+    start = 0
+    res = ""
+    freq = {}
+    i = 0
+    while i < len(s):
+        c = s[i]
+        if c in freq:
+            freq[c] += 1
+        else:
+            freq[c] = 1
+        if len(freq) == k + 1:
+            if i - start > len(res):
+                res = s[start:i]
+            while len(freq) > k:
+                c = s[start]
+                if freq[c] == 1:
+                    freq.pop(c, None)
+                else:
+                    freq[c] -= 1
+                start += 1
+        i += 1
+
+    if len(freq) <= k and len(s) - start > len(res):
+        return s[start:]
+
+    return res
