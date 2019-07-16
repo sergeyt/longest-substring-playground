a82e799956e51b5e1bab71a939a91f44a91e1216
sergeyt <stodyshev@gmail.com>
2019-07-16 22:12:35 +0700

    basic tests

--- python-project/impl.py
+++ python-project/impl.py
@@ -1,5 +1,3 @@
-def count_all_stars(galaxies):
-    total_stars = 0
-    for stars in galaxies:
-        total_stars = stars  # fix me!
-    return total_stars
+# k is maximum number of unique chars
+def longest_substring(s, k = 2):
+    return s
--- python-project/test_impl.py
+++ python-project/test_impl.py
@@ -1,18 +1,4 @@
-from universe import count_all_stars
-import builtins
-
-
-sum_builtin_used = False
-
-
-def new_sum(x):
-    global sum_builtin_used
-    sum_builtin_used = True
-    return orig_sum(x)
-
-
-orig_sum = builtins.sum
-builtins.sum = new_sum
+from impl import longest_substring
 
 
 def send_msg(channel, msg):
@@ -25,34 +11,30 @@ def success():
 
 def fail():
     print("TECHIO> success false")
-    
 
-def test_count_all_stars():
+
+def test_case(input, expected):
+    actual = longest_substring(input)
+    assert actual == expected, "Running longest_substring('{0}')... Expected {1}, got {2}".format(
+        input, expected, actual)
+
+
+def test_longest_substring():
     try:
-        count1 = count_all_stars([2, 3])
-        assert count1 == 5, "Running count_all_stars([2, 3])... Expected 5, got {}".format(count1)
-        count2 = count_all_stars([9, -3])
-        assert count2 == 6, "Running count_all_stars([9, -3])... Expected 6, got {}".format(count2)
+        test_case(None, None)
+        test_case("", "")
+        test_case("a", "a")
+        test_case("abc", "ab")
+        test_case("abcc", "bcc")
+        test_case("aabbcc", "aabb")
+        test_case("ababcc", "abab")
+        test_case("aaabbcc", "aaabb")
+        test_case("aaaabbccccc", "bbccccc")
         success()
-
-        if sum_builtin_used:
-            send_msg("My personal Yoda, you are. 🙏", "* ● ¸ .　¸. :° ☾ ° 　¸. ● ¸ .　　¸.　:. • ")
-            send_msg("My personal Yoda, you are. 🙏", "           　★ °  ☆ ¸. ¸ 　★　 :.　 .   ")
-            send_msg("My personal Yoda, you are. 🙏", "__.-._     ° . .　　　　.　☾ ° 　. *   ¸ .")
-            send_msg("My personal Yoda, you are. 🙏", "'-._\\7'      .　　° ☾  ° 　¸.☆  ● .　　　")
-            send_msg("My personal Yoda, you are. 🙏", " /'.-c    　   * ●  ¸.　　°     ° 　¸.    ")
-            send_msg("My personal Yoda, you are. 🙏", " |  /T      　　°     ° 　¸.     ¸ .　　  ")
-            send_msg("My personal Yoda, you are. 🙏", "_)_/LI")
-        else:
-            send_msg("Kudos 🌟", "Did you know that you could use the sum function? Try it!")
-            send_msg("Kudos 🌟", "")
-            send_msg("Kudos 🌟", "galaxies = [37, 3, 2]")
-            send_msg("Kudos 🌟", "total_stars = sum(galaxies)  # 42")
     except AssertionError as e:
         fail()
         send_msg("Oops! 🐞", e)
-        send_msg("Hint 💡", "Did you properly accumulate all stars into 'total_stars'? 🤔")
 
 
 if __name__ == "__main__":
-    test_count_all_stars()
+    test_longest_substring()
